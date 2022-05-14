94110568d5744821b462fa4e7735ebde05736234
Shawn M Moore <code@sartak.org>
2022-05-14 11:28:47 -0400

    Add CssBaseline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
+import React from "react";
 import type { AppProps } from "next/app";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
