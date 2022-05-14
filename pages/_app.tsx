d2c967ef37bb283494d285fafe8d0f983850fe7b
Shawn M Moore <code@sartak.org>
2022-05-14 11:30:52 -0400

    Add dark mode theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,14 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "../lib/theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />
-    </React.Fragment>
+    </ThemeProvider>
   );
 }
