37d26b5ddc25b6c0268d14f33ec6871c072b1358
Shawn M Moore <code@sartak.org>
2022-05-14 12:32:35 -0400

    Load trees where parent=0 on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>Hello world</h1>;
+import { Tree, getTreesFromParent } from "../lib/trees";
+import { Trees } from "../components/Trees";
+
+type Props = {
+  trees: Array<Tree>;
+};
+
+export default function Home({ trees }: Props) {
+  return <Trees trees={trees} />;
+}
+
+export async function getServerSideProps() {
+  const trees = getTreesFromParent(0);
+  return { props: { trees } };
 }
