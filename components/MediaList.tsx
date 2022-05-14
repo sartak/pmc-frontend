3d6460d6f060c877480c4c3318286dee18ea57ff
Shawn M Moore <code@sartak.org>
2022-05-14 17:31:12 -0400

    Improve media list layout

--- components/MediaList.tsx
+++ components/MediaList.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import React from "react";
-import { List, ListItem, ListItemButton } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { Media } from "../lib/media";
+import { formatDuration } from "./Duration";
 
 type Props = {
   media: Array<Media>;
@@ -14,7 +22,29 @@ export const MediaList = ({ media }: Props) => {
         <Link key={media.id} href={`/media/${media.id}`}>
           <ListItem>
             <ListItemButton>
-              {media.label_en || media.label_ja || media.label_can}
+              <ListItemText
+                primary={
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div>
+                      {media.label_en || media.label_ja || media.label_can}
+                    </div>
+                    {media.durationSeconds && (
+                      <div>
+                        <Typography variant="caption">
+                          {formatDuration(media.durationSeconds)}
+                        </Typography>
+                      </div>
+                    )}
+                  </Box>
+                }
+                secondary={<React.Fragment>{media.identifier}</React.Fragment>}
+              />
             </ListItemButton>
           </ListItem>
         </Link>
