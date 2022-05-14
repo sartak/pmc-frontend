624e06239809349985180f38f7b246876284abcf
Shawn M Moore <code@sartak.org>
2022-05-14 12:31:41 -0400

    Add notistack

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import { AppProps } from "next/app";
+import { SnackbarProvider } from "notistack";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../lib/theme";
 import "@fontsource/roboto/300.css";
@@ -12,7 +13,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Component {...pageProps} />
+      <SnackbarProvider>
+        <Component {...pageProps} />
+      </SnackbarProvider>
     </ThemeProvider>
   );
 }
