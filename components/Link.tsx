6474d684f1adbf366248ffc67baee8f22c1802aa
Shawn M Moore <code@sartak.org>
2022-05-14 13:10:44 -0400

    Add a Link component

---
+++ components/Link.tsx
@@ -0,0 +1,19 @@
+import NextLink from "next/link";
+import React from "react";
+import { Link as MUILink } from "@mui/material";
+
+type Props = {
+  href: string;
+  children: React.ReactNode;
+  color?: string;
+};
+
+export const Link = (props: Props) => {
+  return (
+    <NextLink href={props.href}>
+      <MUILink href="#" color={props.color} underline="hover">
+        {props.children}
+      </MUILink>
+    </NextLink>
+  );
+};
