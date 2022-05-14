4dd6f645e5099abb934ccc506d2f0fa76c349851
Shawn M Moore <code@sartak.org>
2022-05-14 13:28:17 -0400

    Type and loaders for media

---
+++ lib/media.ts
@@ -0,0 +1,33 @@
+import { prepare } from "./sql";
+
+export type MediaType = string;
+export type Media = {
+  id: number;
+  type: MediaType;
+  treeId: number;
+  identifier: string | null;
+  label_en: string | null;
+  label_ja: string | null;
+  label_can: string | null;
+  streamable: boolean;
+  tags: string;
+  sort_order: number | null;
+  path: string;
+  checksum: string | null;
+  materialized_path: string | null;
+  spoken_langs: string | null;
+  subtitle_langs: string | null;
+  durationSeconds: number | null;
+  skip1Start: number | null;
+  skip1End: number | null;
+  skip2Start: number | null;
+  skip2End: number | null;
+};
+
+export const getMedia = (id: number): Media =>
+  prepare("SELECT * FROM media WHERE id=?").get(id);
+
+export const getMediaFromTree = (treeId: number): Array<Media> =>
+  prepare(
+    "SELECT * FROM media WHERE treeId=? ORDER BY media.sort_order IS NULL, media.sort_order ASC, media.rowid ASC"
+  ).all(treeId);
