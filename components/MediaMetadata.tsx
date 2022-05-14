6f0b00c6d9ea1fe4c2e031cd1a3b1b91da49635a
Shawn M Moore <code@sartak.org>
2022-05-14 17:17:22 -0400

    Improve display of media metadata

--- components/MediaMetadata.tsx
+++ components/MediaMetadata.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Chip, Divider, Stack } from "@mui/material";
+import { Card, Chip, Divider, Stack } from "@mui/material";
 import { Duration } from "./Duration";
 import { LanguageList } from "./LanguageList";
 import { Media } from "../lib/media";
+import { PropList, PropListItem } from "./PropList";
 import { Skip } from "./Skip";
 import { TagList } from "./TagList";
 
@@ -47,70 +48,84 @@ export const MediaMetadata = ({ media }: Props) => {
   const true_duration = calculateTrueDuration(media);
 
   return (
-    <ul>
-      {media.identifier !== null && <li>identifier: {media.identifier}</li>}
-      <li>
-        <Stack
-          direction="row"
-          spacing={1}
-          sx={{ display: "inline-flex" }}
-          divider={<Divider orientation="vertical" flexItem />}
-        >
-          {[
-            [media.label_en, "label_en"],
-            [media.label_ja, "label_ja"],
-            [media.label_can, "label_can"],
-          ]
-            .filter(([label]) => label !== null)
-            .map(([label, lang]) => (
-              <span key={lang}>{label}</span>
-            ))}
-        </Stack>
-      </li>
-      <li>
-        {media.streamable ? (
-          <Chip sx={{ mr: 1 }} label="streamable" />
-        ) : (
-          <Chip sx={{ mr: 1 }} label="unstreamable" color="warning" />
+    <Card>
+      <PropList>
+        <PropListItem
+          value={
+            <Stack
+              direction="row"
+              spacing={1}
+              sx={{ display: "inline-flex" }}
+              divider={<Divider orientation="vertical" flexItem />}
+            >
+              {[
+                [media.label_en, "label_en"],
+                [media.label_ja, "label_ja"],
+                [media.label_can, "label_can"],
+              ]
+                .filter(([label]) => label !== null)
+                .map(([label, lang]) => (
+                  <span key={lang}>{label}</span>
+                ))}
+            </Stack>
+          }
+        />
+        {media.identifier !== null && (
+          <PropListItem label="Identifier" value={media.identifier} />
         )}
-        <TagList tags={tags} />
-      </li>
-      <li>path: {media.path}</li>
-      {spoken_langs.length > 0 && (
-        <li>
-          spoken_langs: <LanguageList languages={spoken_langs} />
-        </li>
-      )}
-      {subtitle_langs.length > 0 && (
-        <li>
-          subtitle_langs: <LanguageList languages={subtitle_langs} />
-        </li>
-      )}
-      <li>
-        duration:{" "}
-        <Stack direction="row" spacing={1} sx={{ display: "inline-block" }}>
-          {media.durationSeconds === null ? (
-            <span>unknown</span>
-          ) : (
-            <Duration seconds={media.durationSeconds} />
-          )}
-          {(media.skip1Start !== null || media.skip1End !== null) && (
-            <Skip start={media.skip1Start} end={media.skip1End} />
-          )}
-          {(media.skip2Start !== null || media.skip2End !== null) && (
-            <Skip start={media.skip2Start} end={media.skip2End} />
-          )}
-          {true_duration !== null &&
-            (media.skip1Start !== null ||
-              media.skip1End !== null ||
-              media.skip2Start !== null ||
-              media.skip2End !== null) && (
-              <span>
-                → <Duration seconds={true_duration} />
-              </span>
-            )}
-        </Stack>
-      </li>
-    </ul>
+        <PropListItem
+          value={
+            <React.Fragment>
+              {media.streamable ? (
+                <Chip sx={{ mr: 1 }} label="streamable" />
+              ) : (
+                <Chip sx={{ mr: 1 }} label="unstreamable" color="warning" />
+              )}
+              <TagList tags={tags} />
+            </React.Fragment>
+          }
+        />
+        <PropListItem label="Path" value={media.path} />
+        {spoken_langs.length > 0 && (
+          <PropListItem
+            label="Spoken langs"
+            value={<LanguageList languages={spoken_langs} />}
+          />
+        )}
+        {subtitle_langs.length > 0 && (
+          <PropListItem
+            label="Subtitle langs"
+            value={<LanguageList languages={subtitle_langs} />}
+          />
+        )}
+        <PropListItem
+          label="Duration"
+          value={
+            <Stack direction="row" spacing={1} sx={{ display: "inline-block" }}>
+              {media.durationSeconds === null ? (
+                <span>unknown</span>
+              ) : (
+                <Duration seconds={media.durationSeconds} />
+              )}
+              {(media.skip1Start !== null || media.skip1End !== null) && (
+                <Skip start={media.skip1Start} end={media.skip1End} />
+              )}
+              {(media.skip2Start !== null || media.skip2End !== null) && (
+                <Skip start={media.skip2Start} end={media.skip2End} />
+              )}
+              {true_duration !== null &&
+                (media.skip1Start !== null ||
+                  media.skip1End !== null ||
+                  media.skip2Start !== null ||
+                  media.skip2End !== null) && (
+                  <span>
+                    → <Duration seconds={true_duration} />
+                  </span>
+                )}
+            </Stack>
+          }
+        />
+      </PropList>
+    </Card>
   );
 };
