ffa280bc48accacd77bd65a208312e5bc1b3d2d6
Shawn M Moore <code@sartak.org>
2022-05-15 16:30:38 -0400

    Add a TagListInput component

--- components/TagList.tsx
+++ components/TagList.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Chip, Stack } from "@mui/material";
+import React, { useState } from "react";
+import { Chip, Stack, TextField } from "@mui/material";
 
 type Props = {
   tags: Array<string>;
@@ -11,6 +11,53 @@ export const TagList = ({ tags }: Props) => {
       {tags.map((tag, i) => (
         <Chip key={i} label={tag} />
       ))}
+    </Stack>
+  );
+};
+
+type InputProps = {
+  tags: Array<string>;
+  onChange: (tags: Array<string>) => void;
+};
+
+export const TagListInput = ({ tags, onChange }: InputProps) => {
+  const [addingTag, setAddingTag] = useState<string | null>(null);
+
+  return (
+    <Stack
+      direction="row"
+      spacing={1}
+      sx={{ display: "inline-flex", alignItems: "center", height: "25px" }}
+    >
+      {tags.map((tag, i) => (
+        <Chip
+          key={i}
+          size="medium"
+          label={tag}
+          onDelete={() => onChange(tags.filter((_, j) => i !== j))}
+        />
+      ))}
+      {addingTag === null ? (
+        <Chip
+          variant="outlined"
+          label="add tag"
+          onClick={() => setAddingTag("")}
+        />
+      ) : (
+        <TextField
+          hiddenLabel
+          autoFocus
+          size="small"
+          value={addingTag}
+          onChange={(e) => setAddingTag(e.target.value)}
+          onBlur={() => {
+            if (addingTag !== "") {
+              onChange([...tags, addingTag]);
+            }
+            setAddingTag(null);
+          }}
+        />
+      )}
     </Stack>
   );
 };
